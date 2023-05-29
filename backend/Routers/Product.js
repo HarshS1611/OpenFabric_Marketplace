aa2cc40a0a438aefb9f8e215f1341c647914cd1d
HarshS1611 <harshsingh16d@gmail.com>
2023-05-29 12:39:25 +0530

    middleware integrated

--- backend/Routers/Product.js
+++ backend/Routers/Product.js
@@ -1,25 +1,14 @@
 import express from 'express';
 import authenticateToken from '../Middleware/authMiddleware.js';
 import { createProduct, deleteProduct, getProducts, updateProduct, getProductbyId } from '../Controllers/Product.js';
-import multer from 'multer';
 
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "uploads/");
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, Date.now() + "-" + file.originalname);
-//   },
-// });
-
-// const upload = multer({ storage: storage });
 
 const router = express.Router();
 
 router.get("/", getProducts);
 router.get("/:id", getProductbyId);
-router.post("/", createProduct);
-router.patch("/:id", updateProduct);
-router.delete("/:id", deleteProduct);
+router.post("/",authenticateToken, createProduct);
+router.patch("/:id",authenticateToken, updateProduct);
+router.delete("/:id",authenticateToken, deleteProduct);
 
 export default router;
